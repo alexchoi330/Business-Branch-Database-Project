9e05a90387d4ec28c6bd0556c0394226a50e372d
Alex Choi <alexchoi357@hotmail.co.uk>
2021-09-28 19:27:07 -0700

    Update project_delete.php

--- project_delete.php
+++ project_delete.php
@@ -103,7 +103,7 @@
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example,
 			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_yuxuanhe", "a37063154", "dbhost.students.cs.ubc.ca:1522/stu");
+            $db_conn = OCILogon("ora_alexc330", "a43949767", "dbhost.students.cs.ubc.ca:1522/stu");
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
